439a5d84bbd7b56d25e2be383acb4b14211e67a1
Denzelf-png <79867017+Denzelf-png@users.noreply.github.com>
2021-03-18 10:16:31 -0700

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -15,14 +15,20 @@ def mainProgram():
         print("Please choose from options the following options. Type the number of the choice")
         choice = input("""1. Add to a list,
 2. Return a vaule at a list,
-3. Random Search
-4. Quit  """)
+3. Add a bunch or numbers!
+4. Random Search
+5. Print List
+66. Quit  """)
         if choice == "1":
             addToList()
         elif choice == "2":
             indexValues()
         elif choice == "3":
+            addABunch()
+        elif choice == "4":
             randomSearch()
+        elif choice == "5":
+            print(myList)
         else:
             break
     
@@ -33,9 +39,20 @@ def addToList():
     myList.append(int(newItem))
     #we need to think about errors!
 
+def addABunch():
+    print("We're gonna add a bunch of numbers to your lists!")
+    numToAdd = input("How many new integers would you like to add?  ")
+    numRange = input("And how high would you like these numbers to go?  ")
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is complete!")
+
 def randomSearch():
     print(" oH iM sO rAnDom!!!")
     print(myList[random.randit(0, len(myList)-1)])
+
+def LinearSearch():
+    print("We're going to go through this list one item at a time!")
 
 def indexValues():
     print("At what index position do you want to search?")
